d7e604e6992733883d059d4b5c70b155ac79b0a9
Jorge Coutinho <jorgecsn@id.uff.br>
2025-01-26 23:28:01 -0300

    fix lint

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -1,8 +1,8 @@
 class AccountMailer < ApplicationMailer
-  default from: 'no-reply@bank.com'
+  default from: "no-reply@bank.com"
 
   def welcome_email(bank_account)
     @bank_account = bank_account
-    mail(to: @bank_account.email, subject: 'Welcome to Our Bank')
+    mail(to: @bank_account.email, subject: "Welcome to Our Bank")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,11 @@ Rails.application.routes.draw do
   get "statement", to: "home#statement"
 
   resources :bank_accounts, only: %i[new create] do
-
     resources :bank_transactions, only: %i[new create] do
       collection do
         post :manager_visit
       end
     end
-
   end
 
   resources :sessions, only: %i[new create destroy]
